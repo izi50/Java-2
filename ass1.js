e69586811574e271487a7a41de923bf00476a8db
izi50 <iziren.ogoh@gmail.com>
2024-03-25 10:17:15 +0000

    This is the input string version

---
+++ ass1.js
@@ -0,0 +1,13 @@
+// code checks the datatype that is inputed in the prompt
+wd = prompt("Input a number or a string here");
+if (wd==parseInt(wd)){
+
+document.write ('you inputed a number')
+
+}
+
+else{
+
+document.write('you inputed a string')
+
+}
